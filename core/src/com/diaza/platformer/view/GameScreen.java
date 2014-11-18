f0fd388258f5be65efb32dccf05c027e84c8bf38
Anthony Diaz <diazanthony32@gmail.com>
2014-11-18 15:04:37 -0800

    Platformer Change #2
    Able to run the level

---
+++ core/src/com/diaza/platformer/view/GameScreen.java
@@ -0,0 +1,73 @@
+package com.diaza.platformer.view;
+
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class GameScreen implements Screen{
+
+    public TiledMap map;
+    public OrthogonalTiledMapRenderer renderer;
+    public OrthographicCamera camera;
+
+    public GameScreen() {
+
+        //loads up the map from assets folder
+        map = new TmxMapLoader().load("map/Level_01.tmx");
+
+        //renders the map (1/70 is the unit to pixel size)
+        renderer = new OrthogonalTiledMapRenderer(map, 1/70f);
+
+        //creates a camera with a width of 14*14 units
+        camera = new OrthographicCamera(14f, 14f);
+
+    }
+
+
+    //always running during the game
+    @Override
+    public void render(float delta) {
+
+        //when camera position changes it updates
+        camera.update();
+
+        //sets the view to the camera
+        renderer.setView(camera);
+
+        //renders the render
+        renderer.render();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
